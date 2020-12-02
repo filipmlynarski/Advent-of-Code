95d62f4c388435012d5d3a41eda11e7f4b4d9f54
Filip Mlynarski <fmynarski@gmail.com>
2020-12-02 06:20:26 +0100

    added flag that will print benchmark table in Markdown style

--- benchmark.py
+++ benchmark.py
@@ -9,16 +9,19 @@ from timeit import default_timer
 __author__ = 'Filip Młynarski'
 
 parser = argparse.ArgumentParser(
-    description='This scripts benchmarks advent of code solutions')
-parser.add_argument('-y', '--year', help='Year to benchmark', default=2020)
+    description='This scripts benchmarks Advent of Code solutions')
+parser.add_argument(
+    '-y', '--year', help='Year to benchmark (directory name)', default=2020)
 parser.add_argument(
     '-i', '--interpreter', help='Interpreter to use', default='python')
+parser.add_argument(
+    '-md', help='Format table in Markdown syntax', action='store_true')
 args = parser.parse_args()
 
 
-def format_row(row):
+def format_row(row_):
     formatted = []
-    for cell, fmt in zip(row, formatting.values()):
+    for cell, fmt in zip(row_, formatting.values()):
         if isinstance(cell, float):
             cell = f'{round(cell, fmt["round"]):0<{fmt["round"]+2}}s'
             formatted.append(f'{cell:>{fmt["width"]}}')
@@ -27,25 +30,44 @@ def format_row(row):
     return f'|{"|".join(formatted)}|'
 
 
+print(f'Benchmarking year {args.year} '
+      f'[interpreter: {args.interpreter}] '
+      f'[CPU: {platform.processor()}]')
+
 match = re.compile('\d{2}.py').match
 os.chdir(str(args.year))
 files = filter(match, os.listdir('.'))
 
-# TODO implement competition place (will do only if I get any :P)
 formatting = {
     'day': {'width': 5},
     'part one': {'width': 10, 'round': 3},
     'part two': {'width': 10, 'round': 3},
     'total time': {'width': 10, 'round': 3},
 }
-separators = ("-" * col_format["width"] for col_format in formatting.values())
+stats = dict()
+if os.path.isfile('stats.txt'):
+    formatting.update({'stats': {'width': 11}})
+    stats = {
+        day: f'#{part_1}/#{part_2}'
+        for day, part_1, part_2 in map(str.split, open('stats.txt').readlines())
+    }
+
+separators = ('-' * col_format['width'] for col_format in formatting.values())
 row_sep = f'+{"+".join(separators)}+'
-print(f'Benchmarking year {args.year} '
-      f'[interpreter: {args.interpreter}] '
-      f'[CPU: {platform.processor()}]')
-print(row_sep)
+if not args.md:
+    print(row_sep)
 print(format_row(formatting.keys()))
-print(row_sep.replace('-', '='))
+
+if args.md:
+    md_separators = []
+    for col_format in formatting.values():
+        if 'round' in col_format:
+            md_separators.append(f'{"-" * (col_format["width"] - 1)}:')
+        else:
+            md_separators.append(f':{"-" * (col_format["width"] - 2)}:')
+    print(f'|{"|".join(md_separators)}|')
+else:
+    print(row_sep.replace('-', '='))
 
 total_time = 0
 interpreter = args.interpreter
@@ -60,8 +82,11 @@ for file_name in sorted(files):
     part_2_time = default_timer() - start - part_1_time
     total_time += part_1_time + part_2_time
 
-    print(format_row([day.lstrip('0'), part_1_time, part_2_time, '']))
-    print(row_sep)
+    row = [day.lstrip('0'), part_1_time, part_2_time, '', stats.get(day, '')]
+    print(format_row(row))
+    if not args.md:
+        print(row_sep)
 
-print(format_row(['', '', '', total_time]))
-print(row_sep)
+print(format_row(['', '', '', total_time, '']))
+if not args.md:
+    print(row_sep)
