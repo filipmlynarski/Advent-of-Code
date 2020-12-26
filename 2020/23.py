1d5881ce6eb8c0836cf5a6f216310115ddfb9a60
Filip Mlynarski <fmynarski@gmail.com>
2020-12-26 07:35:04 +0100

    day 23

--- 2020/23.py
+++ 2020/23.py
@@ -1,17 +1,26 @@
-puzzle = (list(map(int, list(open('puzzle/23.in').read()))) +
-          list(range(10, 31)))
-sorted_ = list(sorted(puzzle))
-linked_cups = dict(zip(sorted_, [sorted_[-1]] + sorted_[:-1]))
+def play(cups, rounds):
+    linked_cups = dict(zip(cups, cups[1:] + [cups[0]]))
+    current_cup = cups[0]
+    for _ in range(rounds):
+        stack = [linked_cups[current_cup]]
+        for _ in range(2):
+            stack.append(linked_cups[stack[-1]])
+        next_cup = current_cup - 1 or len(cups)
+        while next_cup in stack or next_cup == 0:
+            next_cup = (next_cup - 1) or len(cups)
+        linked_cups[current_cup] = linked_cups[stack[-1]]
+        linked_cups[stack[-1]] = linked_cups[next_cup]
+        linked_cups[next_cup] = stack[0]
+        current_cup = linked_cups[current_cup]
+    return linked_cups
 
-for idx_ in range(30):
-    current_cup = puzzle.pop(0)
-    stack, rest = puzzle[:3], puzzle[3:]
-    next_cup = linked_cups[current_cup]
-    while next_cup in stack:
-        next_cup = linked_cups[next_cup]
-    idx = rest.index(next_cup)
-    puzzle = [*rest[:idx], next_cup, *stack, *rest[idx+1:], current_cup]
-    print(','.join(map(str, puzzle[-(idx_ + 1):] + puzzle[:-(idx_ + 1)])))
 
-one_idx = puzzle.index(1)
-# print(*puzzle[one_idx+1:] + puzzle[:one_idx], sep='')
+puzzle = list(map(int, open('puzzle/23.in').read()))
+game_1_cups = play(puzzle, 100)
+ans = [game_1_cups[1]]
+for _ in range(7):
+    ans.append(game_1_cups[ans[-1]])
+print(''.join(map(str, ans)))
+
+game_2 = play(puzzle + list(range(10, 1_000_001)), 10_000_000)
+print(game_2[1] * game_2[game_2[1]])
