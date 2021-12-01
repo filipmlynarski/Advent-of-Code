5a6e9af9caa40d10d6bad733aa6289d996f7d072
Filip Mlynarski <fmynarski@gmail.com>
2021-12-01 06:35:21 +0100

    year 2021 day 01

--- 2021/01.py
+++ 2021/01.py
@@ -1,29 +1,16 @@
 import sys
 sys.dont_write_bytecode = True
 from utils import *
-"""
-Strings, lists, dicts:
-lmap, ints, positive_ints, floats, positive_floats, words
-Data structures:
-Linked, UnionFind
-dict: d.keys(), d.values(), d.items()
-deque: q[0], q.append and q.popleft
-List/Vector operations:
-GRID_DELTA, OCT_DELTA
-lget, lset, fst, snd
-padd, pneg, psub, pmul, pdot, pdist1, pdist2sq, pdist2
-Matrices:
-matmat, matvec, matexp
-binary_search(lambda x: x > 90000000000)  # -> 90000000001
-"""
 
 puzzle = open('puzzle/01.in').read()
 all_ints = ints(puzzle)
-lines = puzzle.splitlines()
-lines_ints = lmap(ints, lines)
 
-ans = None
-for line in lines:
-    print(ints(line))
+part_1 = 0
+part_2 = 0
+for idx in range(len(all_ints) - 1):
+    part_1 += all_ints[idx] < all_ints[idx + 1]
+    if idx < len(all_ints) - 2:
+        part_2 += sum(all_ints[idx:idx+3]) < sum(all_ints[idx+1:idx+4])
 
-print(ans)
+print(part_1)
+print(part_2)
