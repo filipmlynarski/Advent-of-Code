7749055c234ad2b6731c15a15d8f6ff65fa4dab8
Felipe Morales <fmynarski@gmail.com>
2022-12-10 16:36:51 +0100

    Advent of Code 2022 day 10 refactor

--- 2022/10.py
+++ 2022/10.py
@@ -4,31 +4,15 @@ sys.dont_write_bytecode = True
 from utils import *
 
 puzzle = open('puzzle/10.in').read()
-lines = puzzle.splitlines()
-
-cycle = 0
-part_1 = 0
-x = 1
-grid = []
-for line in lines:
-    noop = line == 'noop'
-    for _ in range(1 if noop else 2):
+part_1, part_2, cycle, x = 0, [], 0, 1
+for line in puzzle.splitlines():
+    for _ in range(1 if line == 'noop' else 2):
         cycle += 1
-        if abs((len(grid) % 40) - x) < 2:
-            grid.append('#')
-        else:
-            grid.append('.')
+        part_2.append('#' if abs((len(part_2) % 40) - x) < 2 else '.')
         if (cycle - 20) % 40 == 0:
             part_1 += x * cycle
-    if noop:
-        continue
-    val = int(line.split()[-1])
-    x += val
+    if line != 'noop':
+        x += int(line.split()[-1])
 
 time_print(part_1)
-part_2 = []
-for idx, i in enumerate(grid):
-    if idx % 40 == 0:
-        part_2.append([])
-    part_2[-1].append(i)
-time_print('\n' + '\n'.join(''.join(line) for line in part_2))
+time_print('\n' + '\n'.join(''.join(line) for line in chunked(part_2, 40)))
